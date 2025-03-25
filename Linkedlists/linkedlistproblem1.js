9bef3b6b61d0a2d7a1c0e6fbfad7e90b35e0600d
xenexashura125 <xenexashura@gmail.com>
2025-03-25 21:57:37 +0800

    linked list problems

--- Linkedlists/linkedlistproblem1.js
+++ Linkedlists/linkedlistproblem1.js
@@ -9,7 +9,7 @@ class Node {
 class LinkedList {
     constructor() {
         this.head = null;
-        this.tail = null;
+        // this.tail = null;
         this.size = 0;
     }
 
@@ -84,6 +84,78 @@ class LinkedList {
         return console.log(curr2);
     }
 
+    getMidDelete(head = this.head) {
+
+        let curr1 = head;
+        let curr2 = head;
+        let ct = 0;
+        let rc = 0;
+
+        while(curr1) {
+            curr1 = curr1.next;
+            ct += 1;
+        }
+        rc = ct;
+        ct = Math.floor(ct / 2);
+
+        // if (ct % 2 == 0) {
+        //     ct = Math.ceil(ct / 2);
+        // } else {
+        //     ct = Math.ceil(ct / 2) - 1;
+        // }
+        
+        while(ct) {
+
+            if(ct == 1) {
+                curr2.next = curr2.next.next;
+            } else {
+                curr2 = curr2.next;
+            }
+            ct--;
+        }   
+
+    }
+
+    removeAllDuplicates() {
+
+
+
+        const callBack = (curr) => {
+            if (curr && curr.next && curr.value === curr.next.value) {
+                return callBack(curr.next);
+            } else {
+                return curr;
+            }
+        }        
+
+        this.head = callBack(this.head);
+        let curr = this.head;
+
+
+        while (curr.next) {
+
+            const loopHole = callBack(curr.next);
+
+            if (loopHole != undefined) {
+                curr.next = loopHole;
+            }   
+
+            // curr = callBack(curr.next) != undefined && callBack(curr.next);
+            // if(curr?.value == curr.next?.value) {
+            //     curr.next = curr.next.next;
+            //     console.log("Duplicates Detected", curr.value, curr.next.value)
+            // } else {
+            //     console.log("pass")
+            // }
+
+            curr = curr.next;
+        }
+
+        return this.head;
+        // console.log(curr);
+
+    }
+
     reverse(head = this.head) {
         if(this.getSize() == 1) {
             return this.head
@@ -128,37 +200,202 @@ class LinkedList {
 
     }
 
-    rotate(head = this.head) {
-                               
+    // Singly LinkedList
+    rotateLeft(k) {
+        if (!this.head || k <= 0) return; // nothing to do
+
+        // 1. Compute the length and get the tail
+        let tail = this.head;
+        let size = 1;
+        while (tail.next) {
+            tail = tail.next;
+            size++;
+        }
+
+        // 2. Compute effective rotations (if k is larger than size)
+        k = k % size;
+        if (k === 0) return; // no rotation needed
+
+        // 3. Make the list circular
+        tail.next = this.head;
+
+        // 4. Traverse to the new tail (the kth node from the beginning)
+        let newTail = this.head;
+        for (let i = 0; i < k - 1; i++) {
+            newTail = newTail.next;
+        }
+
+        console.log(newTail.next);
+        
+        // newTail.next = new Node(newTail.value + 1);
+
+        // New head is next of newTail
+        let newHead = newTail.next;
+
+        // 5. Break the circle and update head pointer
+        newTail.next = null;
+        this.head = newHead;
+
     }
 
+    rotateRight(k) {
+        if (!this.head || k <= 0) return; // nothing to do
 
-    printNode1(node = this.head, level = 0, prefix1 = "") {
-        if (node != null) {
-            console.log(" ".repeat(level*4)+ node.value + prefix1 );
-            if (node.next) {
+        // 1. Compute the length and get the tail.
+        let tail = this.head;
+        let size = 1;
+        while (tail.next) {
+            tail = tail.next;
+            size++;
+        }
+
+        // 2. Compute effective rotations.
+        k = k % size;
+        if (k === 0) return;
+
+        // 3. Make the list circular.
+        tail.next = this.head;
+        // If using a doubly linked list, update head.prev too:
+        this.head.prev = tail;
+
+        // 4. Find the new tail: it's at position (size - k - 1) from the beginning.
+        let newTail = this.head;
+        for (let i = 0; i < size - k - 1; i++) {
+            newTail = newTail.next;
+        }
+        // 5. The new head is next of the new tail.
+        let newHead = newTail.next;
+
+        // 6. Break the circle.
+        newTail.next = null;
+        if (newHead) {
+            newHead.prev = null;
+        }
+
+        // 7. Update head and tail pointers.
+        this.head = newHead;
+        this.tail = newTail;
+    }
+
+    circleInsertion(value,idx) {
+        if (!this.head || idx <= 0) return; // nothing to do
+
+        let tail = this.head;
+        let size = 1;
+        while(tail.next) {
+            tail = tail.next;
+            size++;
+        }
+
+
+        // 2. Compute effective rotations (if k is larger than size)
+        idx = idx % size;
+        const node = new Node(value);
+        if (idx === 0) {
+            node.next = this.head;
+            this.head = node;
+        } else {            
+            let curr = this.head;
+
+            for (let i = 0; i < idx - 1 ; i++) {
+                curr = curr.next;
+            }
+
+            node.next = curr.next;
+            curr.next = node;
+
+        } 
+        tail.next = null;
+        // this.head = node;
+        this.size++;
+
+    }   
+
+
+
+    printNode1(node = this.head, level = 0, prefix1 = "--->") {
+        // console.log(node.next);
+        if (node.next != null) {
+            // console.log(node.next);
+            if(level > 0) {
+                console.log(" ".repeat(level*4) + node.value + prefix1 );
+            } else {
+                console.log(" ".repeat(level*4) + node.value + prefix1  );
+            } 
+            
+            if (node.next == null) {
+                this.printNode1(node, level+1, "---> null", );
+            } else if (node.next) {
                 this.printNode1(node.next, level+1, "---> ", );
+            } 
+        } 
+        else if (node.next == null) {
+            console.log(" ".repeat(level*4) + node.value + '----> null'  );
+        }
+    }
+
+    printReverse(node = this.tail, level = 0, prefix1 = "null<----") {
+        if (node != null) {
+            if(level > 0) {
+                console.log(" ".repeat(level*4 + 5) + prefix1 + node.value );
+            } else {
+                console.log(" ".repeat(level*4) + prefix1 + node.value );
+            }
+            if (node.prev) {
+                this.printReverse(node.prev, level+1, "<---- ", );
             }
         }
     }
+
+    printList(node) {
+        let result = '';
+        while (node !== null) {
+            result += node.data + ' ';
+            node = node.next;
+        }
+        console.log(result.trim());
+    }
+        
 }
 
 
 let link = new LinkedList();
 
-link.prepend(13);
-link.prepend(21);
-link.prepend(33);
-link.prepend(11);
-link.prepend(18);
-link.prepend(55);
-link.prepend(41);
-link.prepend(25);
-// link.prepend(27);
+
+
+link.prepend(3);
+link.prepend(4);
+link.prepend(5);
+link.prepend(6);
+link.prepend(7);
+link.prepend(8);
+link.prepend(9);
+
+
+// link.printNode1();
+
+// console.log( 7 % 5);
+
+// console.log(link.rotate(link.head,5));
+// link.rotateRight(8)
+// link.rotateLeft(7)
+// link.printNode1();
+// link.printReverse();
+
+// link.circleInsertion(12,9);
 
 link.printNode1();
 
+link.removeAllDuplicates()
 
-// console.log(link.head)
+// link.printReverse();
+
+// // link.getMid(link.head)
+// link.getMidDelete(link.head)
+// link.getMidDelete(link.head)
+
+link.printNode1();
+
+// console.log(lin)
 // console.log(link.tail)
 // link.getMid(link.head)
